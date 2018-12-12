d12eef4179c0517971a8a8d22b6b043eb19a3243
徐 浩 <admin@odata.cc>
2018-12-12 20:13:52 +0800

    Initial commit
    Signed-off-by: 徐 浩 <admin@odata.cc>

---
+++ demo1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+var Pi float64
+
+func init() {
+	Pi = 4 * math.Atan(1) //在init函数中计算Pi值
+}
+func main() {
+	DPi := Pi * Pi
+	fmt.Println(Pi, DPi)
+}
---
+++ emdo.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	n := 4 >> 2
+	//	p := &n
+	//	n++
+	//	*p++
+
+	fmt.Println(n)
+}
+
+//const b int  = 10 //b为常量，10为字面量。
