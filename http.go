54224be388154feb80d0cd09e6f64dc326461ba1
徐 浩 <admin@odata.cc>
2018-12-12 20:13:52 +0800

    Initial commit
    Signed-off-by: 徐 浩 <admin@odata.cc>

---
+++ http.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"github.com/robfig/cron"
+	//	 "io"
+	"net/http"
+	//	 "os"
+	//     "flag"
+)
+
+func main() {
+	spec := "*, *, *, *, *, *" // 每天凌晨2：30
+	c := cron.New()
+	c.AddFunc(spec, func() {
+		//生成client 参数为默认
+		client := &http.Client{}
+
+		//生成要访问的url
+		url := "http://1q.tn/index.php?qqq" //需要监控的url地址
+
+		//提交请求
+		reqest, err := http.NewRequest("GET", url, nil)
+
+		if err != nil {
+			panic(err)
+		}
+
+		//处理返回结果
+		response, _ := client.Do(reqest)
+
+		//将结果定位到标准输出 也可以直接打印出来 或者定位到其他地方进行相应的处理
+		//	stdout := os.Stdout
+		//	_, err = io.Copy(stdout, response.Body)
+
+		//返回的状态码
+		status := response.StatusCode
+
+		fmt.Println(status)
+
+	})
+	c.Start()
+	select {}
+}
